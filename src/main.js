16177c4d47b8f6bd8083663c125ec67f950e6224
Faxriddin <140601430+bakhtiyorovv9@users.noreply.github.com>
2026-04-01 11:35:54 +0500

    tets

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { APP_PORT } from "./configs/app.config.js";
+import { connectDB } from "./configs/db.config.js";
+import apiRouter from "./routes/index.js";
+
+const app = express();
+
+app.use(express.json());
+
+await connectDB();
+
+app.use("/api", apiRouter);
+
+app.all("*splat", (req, res) => {
+  res.status(404).send({
+    success: false,
+    message: `Given URL: ${req.url} not found!`,
+  });
+});
+
+console.log("Hii Faxriddin");
+
+console.log("HIII Dostonchikkkkkkkkkkkkkkkkkk ( ")
+
+app.listen(APP_PORT, () => {
+  console.log(`listening on ${APP_PORT}`);
+});
