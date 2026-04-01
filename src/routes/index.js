3871d39f22fd4498170ae9b8d7e0375f8cf08e3e
Faxriddin <140601430+bakhtiyorovv9@users.noreply.github.com>
2026-04-01 11:27:59 +0500

    test

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import categoryRouter from "./category.router.js";
-import bookRouter from "./book.router.js";
-
-const apiRouter = Router();
-
-apiRouter.use("/categories", categoryRouter).use("/books", bookRouter);
-
-export default apiRouter;
